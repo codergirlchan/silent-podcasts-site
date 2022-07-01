c06b09a27e363a005068dc8e2de859fdc8848c99
Chanelle Francis <codergirlchan@gmail.com>
2022-06-30 20:55:16 -0400

    adding HeaderNav and HeroBanner Component

---
+++ src/components/HeaderNavigation.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <div class="content-wrapper">
+      <img
+        class="logo"
+        src="../assets/sp-logo.png"
+        alt="silent podcasts logo"
+      />
+      <h1>We're anything but Silent</h1>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.content-wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  text-align: center;
+  padding: 10px;
+}
+
+.logo {
+  width: 100px;
+}
+
+h1 {
+  width: 100%;
+  text-transform: uppercase;
+  font-family: impact;
+}
+</style>
---
+++ src/components/HeroBanner.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <video loop autoplay muted controls>
+      <source src="../assets/sp-intro.mp4" type="video/mp4" />
+      Sorry, your browser doesn't support embedded videos.
+    </video>
+  </section>
+</template>
+
+<style scoped>
+video {
+  width: 100%;
+}
+</style>
